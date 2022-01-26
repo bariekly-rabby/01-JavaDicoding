0ea7a5460f45d18bbec936bcbbbc6f11e6780f9f
Bariekly-Rabby <bariekly.rabby@gmail.com>
2022-01-26 11:36:23 +0700

    MEnambah file Latihan01.java

---
+++ src/com/bariekly/javadicoding/latihan/Latihan01.java
@@ -0,0 +1,15 @@
+package com.bariekly.javadicoding.latihan;
+
+public class Latihan01 {
+
+    void sayTahmid(){
+
+        System.out.println("Alhamdulillah");
+    }
+
+    public static void main(String[]args){
+
+
+
+    }
+}
